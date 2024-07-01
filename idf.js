5ecac3dad0bbb8aa45c4a3347c931bcf093d754a
RugungDaimary <rugung4702@gmail.com>
2024-07-02 01:25:13 +0530

    Update Vercel configuration

--- idf.js
+++ idf.js
@@ -1,30 +1,14 @@
-// // Description: This file contains the code to read the IDF values from the IDF.txt file and store them in an array.
-// const fs = require("fs");
-// // Reading the IDF values from the IDF.txt file
-// const idfstr = fs.readFileSync("IDF.txt").toString();
-// // Splitting the IDF values by new line
-// const idf = idfstr.split("\n");
-
-// // Converting the IDF values to numbers
-// for (let i = 0; i < idf.length; i++) {
-//   idf[i] = Number(idf[i]);
-// }
-// // console.log(idf);
-
-// module.exports = idf;
-
+// Description: This file contains the code to read the IDF values from the IDF.txt file and store them in an array.
 const fs = require("fs");
+// Reading the IDF values from the IDF.txt file
+const idfstr = fs.readFileSync("IDF.txt").toString();
+// Splitting the IDF values by new line
+const idf = idfstr.split("\n");
 
-try {
-  // Reading the IDF values from the IDF.txt file
-  const idfstr = fs.readFileSync("IDF.txt").toString();
-
-  // Splitting the IDF values by new line and trimming whitespace
-  const idf = idfstr.split("\n").map((line) => Number(line.trim()));
-
-  // Exporting the IDF array
-  module.exports = idf;
-} catch (err) {
-  console.error("Error reading IDF file:", err);
-  module.exports = [];
+// Converting the IDF values to numbers
+for (let i = 0; i < idf.length; i++) {
+  idf[i] = Number(idf[i]);
 }
+// console.log(idf);
+
+module.exports = idf;
