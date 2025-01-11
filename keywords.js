ae79c3241333caaf17b84374aedce243415b959b
RugungDaimary <rugung4702@gmail.com>
2025-01-11 19:33:45 +0530

    Files Added

--- keywords.js
+++ keywords.js
@@ -1,5 +1,12 @@
+// filepath: /Users/rugungdaimary/Desktop/Mac/Projects/CodeIt_SearchEngine/keywords.js
 const fs = require("fs");
-const keywordsstr = fs.readFileSync("keywords.txt").toString();
-const keywords = keywordsstr.split("\n");
-module.exports = keywords;
+const path = require("path");
 
+// Reading the keywords from the keywords.txt file
+const keywordsPath = path.join(__dirname, "keywords.txt");
+const keywordsStr = fs.readFileSync(keywordsPath, "utf8");
+
+// Splitting the keywords by new line
+const keywords = keywordsStr.split("\n");
+
+module.exports = keywords;
